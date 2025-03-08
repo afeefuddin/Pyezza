bf4c73c703a9ee7ce9bd36472dea20723e373fcc
afeefuddin <afeefud2004din@gmail.com>
2025-03-08 23:05:05 +0530

    Add posthog

--- apps/nextjs/src/components/providers/providers.tsx
+++ apps/nextjs/src/components/providers/providers.tsx
@@ -1,12 +1,15 @@
 "use client";
+import { PostHogProvider } from "@/providers/posthog";
 import { ClerkProvider } from "@clerk/nextjs";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = new QueryClient();
   return (
-    <QueryClientProvider client={queryClient}>
-      <ClerkProvider>{children}</ClerkProvider>
-    </QueryClientProvider>
+    <PostHogProvider>
+      <QueryClientProvider client={queryClient}>
+        <ClerkProvider>{children}</ClerkProvider>
+      </QueryClientProvider>
+    </PostHogProvider>
   );
 }
